dda6584250e71d8bcd4009c9361a886d123f9557
thuuuuuuuuu <s03610242@go.thu.edu.tw>
2026-04-09 15:56:08 +0800

    新增dto欄位不完全暴露

---
+++ tim_BackendTest_MidLevel/Dtos/MyOfficeAcpdResponseDto.cs
@@ -0,0 +1,35 @@
+﻿namespace tim_BackendTest_MidLevel.Dtos
+{
+  public class MyOfficeAcpdResponseDto
+  {
+    public string AcpdSid { get; set; } = string.Empty;
+
+    public string? AcpdCname { get; set; }
+
+    public string? AcpdEname { get; set; }
+
+    public string? AcpdSname { get; set; }
+
+    public string? AcpdEmail { get; set; }
+
+    public byte? AcpdStatus { get; set; }
+
+    public bool? AcpdStop { get; set; }
+
+    public string? AcpdStopMemo { get; set; }
+
+    public string? AcpdLoginId { get; set; }
+
+    public string? AcpdLoginPwd { get; set; }
+
+    public string? AcpdMemo { get; set; }
+
+    public DateTime? AcpdNowDateTime { get; set; }
+
+    public string? AcpdNowId { get; set; }
+
+    public DateTime? AcpdUpddateTime { get; set; }
+
+    public string? AcpdUpdid { get; set; }
+  }
+}
---
+++ tim_BackendTest_MidLevel/Dtos/MyOfficeAcpdUpdateRequestDto.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tim_BackendTest_MidLevel.Dtos
+{
+  public class MyOfficeAcpdUpdateRequestDto
+  {
+    [Required]
+    [StringLength(60)]
+    public string AcpdCname { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(40)]
+    public string AcpdEname { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(40)]
+    public string AcpdSname { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(60)]
+    [EmailAddress]
+    public string AcpdEmail { get; set; } = string.Empty;
+
+    public byte? AcpdStatus { get; set; }
+
+    public bool? AcpdStop { get; set; }
+
+    [StringLength(60)]
+    public string? AcpdStopMemo { get; set; }
+
+    [Required]
+    [StringLength(30)]
+    public string AcpdLoginId { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(60)]
+    public string AcpdLoginPwd { get; set; } = string.Empty;
+
+    [StringLength(600)]
+    public string? AcpdMemo { get; set; }
+
+    [Required]
+    [StringLength(20)]
+    public string AcpdUpdid { get; set; } = string.Empty;
+  }
+}
