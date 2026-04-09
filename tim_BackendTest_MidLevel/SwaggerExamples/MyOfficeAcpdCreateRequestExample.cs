38ea94d6a0efe7a4c074944a9b769c7c0121614d
thuuuuuuuuu <s03610242@go.thu.edu.tw>
2026-04-09 16:23:43 +0800

    新增Swagger的json範例

---
+++ tim_BackendTest_MidLevel/SwaggerExamples/MyOfficeAcpdCreateRequestExample.cs
@@ -0,0 +1,26 @@
+﻿using Swashbuckle.AspNetCore.Filters;
+using tim_BackendTest_MidLevel.Dtos;
+
+namespace tim_BackendTest_MidLevel.SwaggerExamples
+{
+  public class MyOfficeAcpdCreateRequestExample : IExamplesProvider<MyOfficeAcpdCreateRequestDto>
+  {
+    public MyOfficeAcpdCreateRequestDto GetExamples()
+    {
+      return new MyOfficeAcpdCreateRequestDto
+      {
+        AcpdCname = "王小明",
+        AcpdEname = "Wang",
+        AcpdSname = "Tim",
+        AcpdEmail = "tim@example.com",
+        AcpdStatus = 1,
+        AcpdStop = false,
+        AcpdStopMemo = "",
+        AcpdLoginId = "tim001",
+        AcpdLoginPwd = "P@ssw0rd",
+        AcpdMemo = "swagger create test",
+        AcpdNowId = "SYSTEM"
+      };
+    }
+  }
+}
